005cf28e8072fc7363411e8e4e088b97bcf5f903
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2023-01-02 12:40:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 912 ms, Memory: 63852 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Pos {
+    public int x;
+    public int y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        int count = Integer.parseInt(input.readLine());
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(input.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        List<Pos> poses = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            String pox[] = input.readLine().split(" ");
+            int x = Integer.parseInt(pox[0]);
+            int y = Integer.parseInt(pox[1]);
+
+            poses.add(new Pos(x, y));
+        }
+
+        poses.sort((a, b) -> {
+            if (a.x == b.x) return a.y - b.y;
+            return a.x - b.x;
+        });
+
+        for (int i = 0; i < count; i++) {
+            sb.append(poses.get(i).x + " " + poses.get(i).y);
+            sb.append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
+
+
