31819c02c529f1f52160571bed8301a9dd615e14
snowlight-aemt <it.s.aemt@gmail.com>
2023-01-31 13:05:19 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 688 ms, Memory: 86548 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -1,75 +1,74 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
-    
-        static int[] temp;
-        static int count = 0;
-        static int K;
-        static int result = -1;
-    
-        public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
+public class Main
+{
+    public static void main(String[] args) throws IOException {
 
-        int num = sc.nextInt();
-        int[] arr = new int[num];
-        K = sc.nextInt();
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputOne = input.readLine().split(" ");
+        int count = Integer.valueOf(inputOne[0]);
+        cnt = Integer.valueOf(inputOne[1]);
 
-        temp = new int[num];
+        arrTemp = new int[count];
+        int[] arr = new int[count];
 
-        for(int i = 0 ; i < num ; i++){
-            arr[i] = sc.nextInt();
+        String[] list = input.readLine().split(" ");
+        for (int i = 0; i < count; i++) {
+            arr[i] = Integer.valueOf(list[i]);
         }
 
-        sc.close();
+        mergeSort(arr, 0, arr.length - 1);
 
-        merge_sort(arr, 0, arr.length - 1);
-        System.out.print(result);
+        System.out.println(result);
+    }
 
-      }
+    public static void mergeSort(int[] arr, int first, int last) {
+        if (result != -1) return;
 
-        static void merge_sort(int A[], int low, int high){
+        if (first < last) {
+            int middle = ((first + last) / 2);
+            mergeSort(arr, first, middle);
+            mergeSort(arr, middle + 1, last);
+            merge(arr, first, middle, last);
+        }
+    }
 
-            if(low < high){
-                int mid = (low + high) / 2;
-                merge_sort(A, low, mid);
-                merge_sort(A, mid + 1, high);
-                merge(A, low, mid, high);
+    public static int cnt = 0;
+
+    public static int result = -1;
+
+    public static int[] arrTemp ;
+
+    public static void merge(int[] arr, int first, int middle, int last) {
+        int lfirst = first;
+        int rfirst = middle + 1;
+        int arrTeampIndex = 0;
+
+        while (lfirst <= middle && rfirst <= last) {
+            if (arr[lfirst] < arr[rfirst]) {
+                arrTemp[arrTeampIndex++] = arr[lfirst];
+                lfirst++;
+            } else {
+                arrTemp[arrTeampIndex++] = arr[rfirst];
+                rfirst++;
             }
         }
 
-static void merge(int A[], int low, int mid, int high){
-        int i = low;
-        int j = mid + 1;
-        int t = 0;
+        while (lfirst <= middle)
+            arrTemp[arrTeampIndex++] = arr[lfirst++];
 
-        
-        while(i <= mid && j <= high){
-            if(A[i] <= A[j]){
-                temp[t++] = A[i++];
-            }else{
-                temp[t++] = A[j++];
+        while (rfirst <= last)
+            arrTemp[arrTeampIndex++] = arr[rfirst++];
+
+        int idx = 0;
+        for (int i = first; i <= last; i++) {
+            if (--cnt == 0) {
+                result = arrTemp[idx];
+                break ;
             }
+            arr[i] = arrTemp[idx++];
         }
-
-        while(i <= mid){
-            temp[t++] = A[i++];
-        }
-
-        while(j <= high){
-            temp[t++] = A[j++];
-        }
-
-        t = 0;
-        i = low;
-
-        while(i <= high){
-            count++;
-            if(count==K){
-                result = temp[t];
-                break;
-            }
-            A[i++] = temp[t++];
-        }     
-   }
+    }
 }
