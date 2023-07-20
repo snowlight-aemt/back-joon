eecb0316b78f95c5001bdf94a4b55912ca7bdeca
snowlight-aemt <it.s.aemt@gmail.com>
2023-07-21 08:42:57 +0900

    Update 광물 캐기.java
    수동 업데이트

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 class Solution {
-    static int[][] scoreBoard;
     // [0, 1, 0] ["diamond", "iron", "iron", "iron", "iron", "diamond", "diamond", "iron", "iron", "iron"] 9 
     // [1, 1, 0],  ["iron", "iron", "diamond", "iron", "stone", "diamond", "diamond", "diamond"] 12
     // [1, 0, 1] , ["iron", "iron", "iron", "iron", "diamond", "diamond", "diamond"] 47
@@ -15,32 +13,35 @@ class Solution {
         int pickCnt = Arrays.stream(picks).sum();
         int answer = 0;
 
-        // List<Map<String, Integer>> grid = new ArrayList<>();
-        // grid.add(Map.of("diamond", 1, "iron", 1, "stone", 1));
-        // grid.add(Map.of("diamond", 5, "iron", 1, "stone", 1));
-        // grid.add(Map.of("diamond", 25, "iron", 5, "stone", 1));
-        scoreBoard = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < minerals.length; i += 5) {
-            if (pickCnt == 0) break;
+        List<Map<String, Integer>> grid = new ArrayList<>();
+        grid.add(Map.of("diamond", 1, "iron", 1, "stone", 1));
+        grid.add(Map.of("diamond", 5, "iron", 1, "stone", 1));
+        grid.add(Map.of("diamond", 25, "iron", 5, "stone", 1));
 
-            int _d = 0, _i =0, _s =0;
+        List<List<Integer>> result = new ArrayList<>();
+        int _d = 0, _i = 0, _s = 0;
+        for (int i = 0; i < minerals.length; i++) {
             List<Integer> temp = new ArrayList<>();
-            for (int j = i; j < i + 5; j++) {
-                if (j == minerals.length) break;
-                
-                String mineral = minerals[j];
-                int val = mineral.equals("iron") ? 1 : 
-                    mineral.equals("stone") ? 2 : 0;
-                
-                _d += scoreBoard[0][val];
-                _i += scoreBoard[1][val];
-                _s += scoreBoard[2][val];
+            for (int j = 0; j < picks.length; j++) {
+                Integer score = grid.get(j).get(minerals[i]);
+                temp.add(score);
             }
 
+            _d += temp.get(0);
+            _i += temp.get(1);
+            _s += temp.get(2);
+
+            if ((i != 0 && ((i + 1) % 5) == 0)) {
+                if (pickCnt-- == 0) break;
+
+                result.add(List.of(_d, _i, _s));
+                _d = 0; _i = 0; _s = 0;
+            }
+        }
+
+        if (minerals.length % 5 != 0) {
             result.add(List.of(_d, _i, _s));
-            pickCnt--;
-        }        
+        }
 
         List<List<Integer>> list = result.stream().sorted((o1, o2) -> {
             return o2.get(2) - o1.get(2);
@@ -58,7 +59,7 @@ class Solution {
             } else if (picks[2] > 0) {
                 picks[2]--;
                 answer += list.get(i).get(2);
-
+                
             }
         }
 
